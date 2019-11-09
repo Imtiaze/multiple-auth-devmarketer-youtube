26b56ecd30d9885f298dbd5006b8752b0236ad4f
Ahammed Imtiaze <ahammedimtiaze78@gmail.com>
2019-11-10 01:38:26 +0600

    Page redirection through the middleware done

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,11 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
+        if ($guard == 'admin') {
+            if(Auth::guard($guard)->check())
+                return redirect('/admin');
+        } else {
+            if(Auth::guard($guard)->check())
             return redirect('/home');
         }
 
